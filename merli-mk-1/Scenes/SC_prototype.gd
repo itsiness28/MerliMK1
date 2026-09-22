8a31b3949b6cf821f955d91ce08effa2e61cd19f
Inés Castaño Gijón <inescasgi@outlook.com>
2026-09-22 15:51:23 +0200

    Button that updates variable

---
+++ merli-mk-1/Scenes/SC_prototype.gd
@@ -0,0 +1,16 @@
+extends Node2D
+var totalCount = 0 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_button_pressed() -> void:
+	var totalCount = totalCount + 1
+	print (totalCount)
+	 # Replace with function body.
